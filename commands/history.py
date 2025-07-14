73b9086f3a2507b8650a82a859238768778bec42
Caio <caio@example.com>
2025-07-14 09:43:37 -0300

    Improve formatting in history command

--- commands/history.py
+++ commands/history.py
@@ -1,6 +1,6 @@
 import discord
 from datetime import datetime
-from discord import Embed, app_commands
+from discord import app_commands
 from discord.ext import commands
 from core.bot_workflow.ai_responder import CustomBotData
 from core.bot_workflow.profile_loader import Profile
@@ -23,13 +23,22 @@ class ViewHistoryCommand(commands.Cog):
         )
         embed.set_author(name="Info")
 
-        last_5 = self.ai_bot_data.full_history.backing_history.as_list()[-5:]
-        last_5.reverse()
-        for snapshot in last_5:
-            time = snapshot.sent.strftime("%d/%m/%Y %H:%M:%S")
+        last_msgs = self.ai_bot_data.full_history.backing_history.as_list()[::-1]
+        remaining_allowable_len = 3000
+        MIN_MSGS_PER_EMBED = 5
+        MAX_MSGS_PER_EMBED = 15
+        for i, snapshot in enumerate(last_msgs):
+            time = snapshot.sent.strftime("🕓︎ [%d/%m %H:%M:%S]")
             pending = self.ai_bot_data.full_history.is_pending(snapshot.message_id)
-            name = f"{time} {'(PENDING)' if pending else ''}"
-            value = snapshot.text[:1021]
-            embed.add_field(name=name, value=value, inline=False)
-            
+            name = f"{time} {'- PENDING' if pending else ''}"
+            field_txt = snapshot.text[:1021]
+
+            remaining_allowable_len -= len(snapshot.text)
+            if i >= MIN_MSGS_PER_EMBED and remaining_allowable_len < 0:
+                break
+            if i >= MAX_MSGS_PER_EMBED:
+                break
+
+            embed.add_field(name=name, value=field_txt, inline=False)
+
         await interaction.followup.send(embed=embed)
